f91fac4256ede8b130a4ea73c13c55f6ac4cad91
Maria Marfina <mari.marfina@gmail.com>
2019-12-11 07:21:04 +0300

    add splitAndMerge function

---
+++ MariaMarfina/1-splitAndMerge.js
@@ -0,0 +1,19 @@
+// Write a function splitAndMerge
+// Function accept 2 parameters:str and sp. str is a sentence. sp is a char as separator. 
+// First we need to divide the sentence into words(Use separator space); 
+// and then divide each word into characters(Use separator empty string); 
+// and then merge each characters with the specified sp; 
+// at last merge all the words(Use separator space) and return it.
+
+function splitAndMerge(str, sp) {
+  var wordsFromStr = str.split(' ');
+  var separatedWords = [];
+
+  for (var i = 0; i < wordsFromStr.length; i++) {
+    separatedWords.push(wordsFromStr[i].split('').join(sp));
+  }
+
+  return separatedWords.join(' ');
+}
+
+module.exports = splitAndMerge;
---
+++ MariaMarfina/1-splitAndMerge.test.js
@@ -0,0 +1,6 @@
+var splitAndMerge = require('./1-splitAndMerge');
+
+test('properly splits string and merges it with separator', () => {
+  expect(splitAndMerge("My name is John"," ")).toBe("M y n a m e i s J o h n");
+  expect(splitAndMerge("Hello World!",",")).toBe("H,e,l,l,o W,o,r,l,d,!");
+})
