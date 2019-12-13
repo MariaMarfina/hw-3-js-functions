3ca5c090ef0a813df706e290a82beeecb8061130
Maria Marfina <mari.marfina@gmail.com>
2019-12-13 03:26:00 +0300

    add countDown function

---
+++ MariaMarfina/9-countDown.js
@@ -0,0 +1,17 @@
+// Write function countDown. 
+// Function expects number and logs values one by one till zero with one second delay.
+
+function countDown(num) {
+  var currNum = num;
+  var timerId = setInterval(function() {
+    if (currNum === 0) {
+      clearInterval(timerId);
+    }
+    console.log(currNum);
+    currNum--;
+  }, 1000);
+}
+
+countDown(3)
+
+module.exports = countDown;
